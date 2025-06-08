b9ee18b7bcfb74ff68db41d3cf472e9df4e31133
David Michael II <knowdavidmichael@gmail.com>
2025-06-08 23:12:49 +0100

    add inter text theme with google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tsks_flutter/app.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('assets/fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+  
   runApp(const TsksApp());
 }
--- lib/ui/core/themes/themes.dart
+++ lib/ui/core/themes/themes.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tsks_flutter/ui/core/themes/colors.dart';
 
 abstract final class TsksTheme {
+  static TextTheme _textTheme(TextTheme baseTextTheme) {
+    return GoogleFonts.interTextTheme(baseTextTheme);
+  }
+
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     colorScheme: TsksColors.lightColorScheme,
+    textTheme: _textTheme(lightTheme.textTheme),
   );
 
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     colorScheme: TsksColors.darkColorScheme,
+    textTheme: _textTheme(darkTheme.textTheme),
   );
 }

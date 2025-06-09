69a2dcf8ab7be609083e8d08c082f20bc1206ed1
David Michael II <knowdavidmichael@gmail.com>
2025-06-09 02:31:15 +0100

    configure firebase and register app using firebase_cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tsks_flutter/app.dart';
+import 'package:tsks_flutter/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   LicenseRegistry.addLicense(() async* {
     final license = await rootBundle.loadString('assets/fonts/OFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
   });
-  
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const TsksApp());
 }

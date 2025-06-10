28da7000cb366b189cfa600f1bde4583cec6278a
David Michael II <knowdavidmichael@gmail.com>
2025-06-10 11:41:32 +0100

    configure `usePathUrlStrategy()` to remove `#` from browser address bar and set `GoRouter.optionURLReflectsImperativeAPIs` to true to enable address bar update on navigator push

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,24 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tsks_flutter/app.dart';
 import 'package:tsks_flutter/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    GoRouter.optionURLReflectsImperativeAPIs = true;
+    usePathUrlStrategy();
+  }
+
   LicenseRegistry.addLicense(() async* {
     final license = await rootBundle.loadString('assets/fonts/OFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
   });
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
- 
+
   runApp(const ProviderScope(child: TsksApp()));
 }

95892c66ce656e4eb197f0deeec3e8a9f2aa63b8
David Michael II <knowdavidmichael@gmail.com>
2025-06-13 23:10:45 +0100

    add phone to responsive breakpoints

--- lib/app.dart
+++ lib/app.dart
@@ -37,8 +37,9 @@ class TsksAppView extends ConsumerWidget {
       builder: (context, child) => ResponsiveBreakpoints.builder(
         child: child!,
         breakpoints: [
-          const Breakpoint(start: 0, end: 450, name: MOBILE),
-          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 0, end: 450, name: PHONE),
+          const Breakpoint(start: 451, end: 600, name: MOBILE),
+          const Breakpoint(start: 600, end: 800, name: TABLET),
           const Breakpoint(start: 801, end: 1920, name: DESKTOP),
           const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
         ],

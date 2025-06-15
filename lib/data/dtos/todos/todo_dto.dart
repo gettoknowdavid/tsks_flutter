5557678b7a152b5a84af8176f355fd776f147981
David Michael II <knowdavidmichael@gmail.com>
2025-06-15 07:26:00 +0100

    changes to the todo domain and dto - `collectionUid`: since no todo can be without a collection - `createdAt`: as this is needed

--- lib/data/dtos/todos/todo_dto.dart
+++ lib/data/dtos/todos/todo_dto.dart
@@ -5,61 +5,59 @@ import 'package:tsks_flutter/domain/models/todos/todo.dart';
 final class TodoDto with EquatableMixin {
   const TodoDto({
     required this.uid,
+    required this.collectionUid,
     required this.title,
+    required this.createdAt,
     this.isDone = false,
     this.dueDate,
-    this.createdAt,
     this.updatedAt,
-    this.collectionUid,
   });
 
   factory TodoDto.fromFirestore(String uid, Map<String, dynamic> json) {
     return TodoDto(
       uid: uid,
+      collectionUid: json['collectionUid'] as String,
       title: json['title'] as String,
       isDone: json['isDone'] as bool? ?? false,
       dueDate: json['dueDate'] != null
           ? DateTime.parse(json['dueDate'] as String)
           : null,
-      createdAt: json['createdAt'] != null
-          ? DateTime.parse(json['createdAt'] as String)
-          : null,
+      createdAt: DateTime.parse(json['createdAt'] as String),
       updatedAt: json['updatedAt'] != null
           ? DateTime.parse(json['updatedAt'] as String)
           : null,
-      collectionUid: json['collectionUid'] as String?,
     );
   }
 
   factory TodoDto.fromDomain(Todo todo) {
     return TodoDto(
       uid: todo.uid.getOrCrash,
+      collectionUid: todo.collectionUid.getOrCrash,
       title: todo.title.getOrCrash,
       isDone: todo.isDone,
       dueDate: todo.dueDate,
       createdAt: todo.createdAt,
       updatedAt: todo.updatedAt,
-      collectionUid: todo.collectionUid?.getOrNull,
     );
   }
 
   final String uid;
+  final String collectionUid;
   final String title;
   final bool isDone;
   final DateTime? dueDate;
-  final DateTime? createdAt;
+  final DateTime createdAt;
   final DateTime? updatedAt;
-  final String? collectionUid;
 
   @override
   List<Object?> get props => [
     uid,
+    collectionUid,
     title,
     isDone,
     dueDate,
     createdAt,
     updatedAt,
-    collectionUid,
   ];
 
   Map<String, dynamic> toJson() => <String, dynamic>{
@@ -67,19 +65,20 @@ final class TodoDto with EquatableMixin {
     'title': title,
     'isDone': isDone,
     'dueDate': dueDate?.toIso8601String(),
-    'createdAt': createdAt?.toIso8601String(),
+    'createdAt': createdAt.toIso8601String(),
     'updatedAt': updatedAt?.toIso8601String(),
     'collectionUid': collectionUid,
   };
+
   Todo toDomain() {
     return Todo(
       uid: Uid(uid),
+      collectionUid: Uid(collectionUid),
       title: SingleLineString(title),
       isDone: isDone,
       dueDate: dueDate,
       createdAt: createdAt,
       updatedAt: updatedAt,
-      collectionUid: collectionUid != null ? Uid(collectionUid!) : null,
     );
   }
 
--- lib/domain/models/todos/todo.dart
+++ lib/domain/models/todos/todo.dart
@@ -4,32 +4,37 @@ import 'package:tsks_flutter/domain/core/value_objects/value_objects.dart';
 final class Todo with EquatableMixin {
   const Todo({
     required this.uid,
+    required this.collectionUid,
     required this.title,
+    required this.createdAt,
     this.isDone = false,
     this.dueDate,
-    this.createdAt,
     this.updatedAt,
-    this.collectionUid,
   });
 
-  static Todo empty = Todo(uid: Uid(''), title: SingleLineString(''));
+  static Todo empty = Todo(
+    uid: Uid(''),
+    collectionUid: Uid(''),
+    title: SingleLineString(''),
+    createdAt: DateTime.now(),
+  );
 
   final Uid uid;
+  final Uid collectionUid;
   final SingleLineString title;
   final bool isDone;
   final DateTime? dueDate;
-  final DateTime? createdAt;
+  final DateTime createdAt;
   final DateTime? updatedAt;
-  final Uid? collectionUid;
 
   @override
   List<Object?> get props => [
     uid,
+    collectionUid,
     title,
     isDone,
     dueDate,
     createdAt,
     updatedAt,
-    collectionUid,
   ];
 }

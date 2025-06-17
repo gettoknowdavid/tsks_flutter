b928222257ff97e5b7681cfc4afd52bd9502d940
David Michael II <knowdavidmichael@gmail.com>
2025-06-17 19:26:34 +0100

    add skeleton shade to check mark on collection tile widget

--- lib/ui/todos/widgets/check_mark.dart
+++ lib/ui/todos/widgets/check_mark.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class CheckMark extends StatelessWidget {
   const CheckMark({
@@ -14,17 +15,19 @@ class CheckMark extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color?.withValues(alpha: 0.5),
-        borderRadius: borderRadius,
-      ),
-      child: Center(
-        child: Container(
-          height: dimension,
-          width: dimension,
-          decoration: BoxDecoration(color: color, borderRadius: borderRadius),
-          child: const Icon(Icons.check, size: 13),
+    return Skeleton.shade(
+      child: Container(
+        decoration: BoxDecoration(
+          color: color?.withValues(alpha: 0.5),
+          borderRadius: borderRadius,
+        ),
+        child: Center(
+          child: Container(
+            height: dimension,
+            width: dimension,
+            decoration: BoxDecoration(color: color, borderRadius: borderRadius),
+            child: const Icon(Icons.check, size: 13),
+          ),
         ),
       ),
     );

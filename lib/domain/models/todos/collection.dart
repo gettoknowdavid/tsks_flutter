f9a5afdf105f480cf147690f3432e22e53094604
David Michael II <knowdavidmichael@gmail.com>
2025-06-18 14:29:44 +0100

    update collection and todo models with Timestamp for DateTime to match with Firestore

--- lib/domain/models/todos/collection.dart
+++ lib/domain/models/todos/collection.dart
@@ -4,22 +4,27 @@ import 'package:tsks_flutter/domain/core/value_objects/value_objects.dart';
 final class Collection with EquatableMixin {
   const Collection({
     required this.uid,
+    required this.ownerUid,
     required this.title,
     required this.createdAt,
     this.isFavourite = false,
     this.colorARGB,
     this.iconMap,
+    this.updatedAt,
   });
 
   final Uid uid;
+  final Uid ownerUid;
   final SingleLineString title;
   final bool isFavourite;
   final int? colorARGB;
   final Map<String, dynamic>? iconMap;
   final DateTime createdAt;
+  final DateTime? updatedAt;
 
   static Collection empty = Collection(
     uid: Uid(''),
+    ownerUid: Uid(''),
     title: SingleLineString(''),
     createdAt: DateTime.now(),
   );
@@ -27,10 +32,12 @@ final class Collection with EquatableMixin {
   @override
   List<Object?> get props => [
     uid,
+    ownerUid,
     title,
     isFavourite,
     colorARGB,
     iconMap,
     createdAt,
+    updatedAt,
   ];
 }

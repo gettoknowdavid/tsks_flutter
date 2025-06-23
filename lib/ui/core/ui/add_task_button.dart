f582a65425f546dfa95326673353dd5b513db2be
David Michael II <knowdavidmichael@gmail.com>
2025-06-23 12:27:36 +0100

    update button for easy re-use

--- lib/ui/core/ui/add_task_button.dart
+++ lib/ui/core/ui/add_task_button.dart
@@ -5,7 +5,7 @@ import 'package:tsks_flutter/ui/tasks/providers/task_form/task_form_notifier.dar
 import 'package:tsks_flutter/ui/tasks/widgets/task_extensions.dart';
 
 class AddTaskButton extends ConsumerWidget {
-  const AddTaskButton({super.key, this.onPressed, this.dimension = 56});
+  const AddTaskButton({super.key, this.onPressed, this.dimension = 52});
 
   final VoidCallback? onPressed;
   final double dimension;
@@ -16,29 +16,30 @@ class AddTaskButton extends ConsumerWidget {
     final decoration = ShapeDecoration(
       color: colors.primary,
       shape: const RoundedSuperellipseBorder(
-        borderRadius: BorderRadiusGeometry.all(Radius.circular(16)),
+        borderRadius: BorderRadiusGeometry.all(Radius.circular(14)),
       ),
     );
-    return IconButton(
-      onPressed: () {
-        if (onPressed != null) return onPressed?.call();
+    final fixedSizeDimension = dimension * 1.05;
+    return SizedBox.square(
+      dimension: dimension,
+      child: IconButton(
+        onPressed: () {
+          if (onPressed != null) return onPressed?.call();
 
-        final id = ref.read(routerConfigProvider).state.pathParameters['id'];
-        if (id != null) {
-          ref.read(taskFormNotifierProvider.notifier).collectionChanged(id);
-        }
+          final id = ref.read(routerConfigProvider).state.pathParameters['id'];
+          if (id != null) {
+            ref.read(taskFormNotifierProvider.notifier).collectionChanged(id);
+          }
 
-        context.openTaskEditor();
-      },
-      style: IconButton.styleFrom(
-        fixedSize: const Size.square(70),
-        iconSize: 16,
-      ),
-      icon: Ink(
-        decoration: decoration,
-        width: dimension,
-        height: dimension,
-        child: Icon(Icons.add, color: colors.onPrimary),
+          context.openTaskEditor();
+        },
+        style: IconButton.styleFrom(fixedSize: Size.square(fixedSizeDimension)),
+        icon: Ink(
+          decoration: decoration,
+          width: dimension,
+          height: dimension,
+          child: Icon(Icons.add, color: colors.onPrimary),
+        ),
       ),
     );
   }

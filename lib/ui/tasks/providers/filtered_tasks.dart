fb0b378a9ee4140e351ecf5d8d6ed538151fe639
David Michael II <knowdavidmichael@gmail.com>
2025-06-23 12:29:26 +0100

    create filtered tasks provider Filters tasks based on the `isDone` value of the task

---
+++ lib/ui/tasks/providers/filtered_tasks.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:tsks_flutter/models/tasks/task.dart';
+import 'package:tsks_flutter/ui/tasks/providers/tasks_notifier.dart';
+
+part 'filtered_tasks.g.dart';
+
+@riverpod
+/// Listens to the main [TasksNotifier] and its provider -
+/// [tasksNotifierProvider] and filters the result based on the [isDone] value
+/// provided by the user.ref
+///
+/// To get undone tasks, set the [isDone] value to `false`.
+///
+/// To get done tasks, set the [isDone] value to `true`.
+///
+Stream<List<Task?>> filteredTasks(Ref ref, String? collectionId, bool isDone) {
+  // If collectionId is null, immediately return an empty stream.
+  if (collectionId == null) return Stream.value([]);
+
+  // Create a broadcast StreamController.
+  final controller = StreamController<List<Task?>>.broadcast();
+
+  // Ensure the controller is closed when the provider is disposed.
+  ref.onDispose(controller.close);
+
+  // Listen to the TasksNotifierProvider.
+  ref.listen(
+    tasksNotifierProvider(collectionId),
+    (previous, next) {
+      // Filter the tasks
+      final filteredTasks = next.valueOrNull
+          ?.where((task) => task != null && task.isDone == isDone)
+          .toList();
+
+      // Add the filtered list to your stream
+      controller.add(filteredTasks ?? []);
+    },
+    fireImmediately: true,
+  );
+
+  // Return the stream from the controller.
+  return controller.stream;
+}
